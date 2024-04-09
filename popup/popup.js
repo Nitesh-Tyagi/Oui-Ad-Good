e4e23a0ba6b41df6fc67a2590ad0af89956ff7b0
Nitesh-Tyagi <tyagipsgn@gmail.com>
2024-04-09 21:20:29 +0530

    updated popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleSwitch = document.getElementById('toggleSwitch');
-  
-    chrome.runtime.sendMessage({action: "getStatus"}, function(response) {
-        toggleSwitch.checked = response.enabled;
-        updateStatusText(response.enabled);
-    });
-  
-    toggleSwitch.addEventListener('change', function () {
-        const enabled = toggleSwitch.checked;
-    
-        chrome.runtime.sendMessage({action: "updateEnabledState", enabled: enabled}, function(response) {
-            console.log("Update response:", response.message);
-            updateStatusText(enabled);
-        });
-    });
+const toggleSwitch = document.getElementById('toggleSwitch');
+const toggleLabel = document.getElementById('toggleLabel');
 
-  
-    function updateStatusText(enabled) {
-        document.getElementById('toggleLabel').textContent = enabled ? 'Social Media For Good: Enabled' : 'Social Media For Good: Disabled';
-    }
-  });
-  
+chrome.runtime.sendMessage({action: "getStatus"}, function(response) {
+    toggleSwitch.checked = response.enabled;
+    updateStatusText(response.enabled);
+});
+
+toggleSwitch.addEventListener('change', function () {
+    const enabled = toggleSwitch.checked;
+
+    chrome.runtime.sendMessage({action: "updateEnabledState", enabled: enabled}, function(response) {
+        console.log("Update response:", response.message);
+        updateStatusText(enabled);
+    });
+});
+
+
+function updateStatusText(enabled) {
+    toggleLabel.textContent = enabled ? 'Social Media For Good: Enabled' : 'Social Media For Good: Disabled';
+}
